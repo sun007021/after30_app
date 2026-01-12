1e972a37dda306e532bf35f0d8d2fce61d5a0877
sun <sun007021@gmail.com>
2026-01-12 22:39:23 +0900

    fix: 배포용 백엔드 API URL 변경 누락 수정

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,7 +1,7 @@
 class ApiConfig {
   // 로컬 백엔드 (Android 에뮬레이터는 10.0.2.2 = Mac의 localhost)
-  static const String baseUrl = 'http://10.0.2.2:8000/';
-  //static const String baseUrl = 'https://api.pysun.kr/';
+  //static const String baseUrl = 'http://10.0.2.2:8000/';
+  static const String baseUrl = 'https://api.pysun.kr/';
   static const AuthMode authMode = AuthMode.bearer;
   static const String kakaoRedirectUri =
       'kakao54b330c450ea6d30f52c96937e871989://oauth';
