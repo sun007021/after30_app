47ec90f1770955bcac65698e2aea22a3e9bfa3f1
sun <sun007021@gmail.com>
2026-09-22 23:18:16 +0900

    fix: 로그인 직후 전화번호 등록 팝업 제거(D11)
    HomePage(checkPhoneRegistration: true)가 더 이상 전화번호 등록 팝업을
    띄우지 않는다. 파라미터 자체는 로그인/가입 화면(W3/W6 소유)이 계속
    넘기더라도 컴파일이 깨지지 않도록 @Deprecated로 남기고 무시한다.
    전화번호 등록 유도는 가족 기능 사용 시점으로 옮긴다(W8).

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:after30/features/home/ui/home_content.dart';
 import 'package:after30/features/common/navigationBar.dart';
-import 'package:after30/features/common/widgets/phone_register_dialog.dart';
 
 class HomePage extends StatefulWidget {
+  /// 로그인 직후 전화번호 등록 팝업을 띄우던 옛 플래그.
+  ///
+  /// D11(2026-09-22) 결정으로 로그인 직후에는 더 이상 전화번호 등록을
+  /// 요구하지 않는다(가족 기능을 쓸 때 유도하는 방식으로 대체, W8 참고).
+  /// 로그인/가입 화면(W3/W6 소유)이 이 파라미터를 계속 넘기더라도 컴파일이
+  /// 깨지지 않도록 값은 무시한다.
+  @Deprecated('로그인 직후 전화번호 팝업은 제거됨(D11). 가족 기능에서 유도한다.')
   final bool checkPhoneRegistration;
 
   const HomePage({super.key, this.checkPhoneRegistration = false});
@@ -20,13 +26,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     _loadUserInfo();
-    if (widget.checkPhoneRegistration) {
-      _checkPhoneRegistration();
-    }
-  }
-
-  Future<void> _checkPhoneRegistration() async {
-    await PhoneRegisterDialog.checkAndShowIfNeeded(context);
   }
 
   Future<void> _loadUserInfo() async {
